d874410ce0932350b10ebe600ca23407925fa325
Jeremiah Omoike <jeremiah.omoike@stu.ocsb.ca>
2022-10-13 19:01:52 +0000

    integer program

---
+++ integer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Created By: Jeremiaah omoike
+# Date: Oct 11, 2022
+# This program tells you what type of integer you put in ( odd or even)
+
+
+def main():
+
+    # User input
+    print()
+    user_input = int(input("please input an integer: "))
+
+    # Number assigner
+
+    if user_input > 0:
+
+        print("{} is a positive number".format(user_input))
+
+    elif user_input < 0:
+
+        print("{} is a negative number".format(user_input))
+
+    else:
+
+        print("{} is 0".format(user_input))
+
+
+if __name__ == "__main__":
+
+    main()
